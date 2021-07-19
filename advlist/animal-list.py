8331a0416596e51220be073ebe79d706935a5025
Thad Murillo <cheesecakethad@gmail.com>
2021-07-19 19:26:17 +0000

    more list stuff

---
+++ advlist/animal-list.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+
+animals = [Fox,Fly,Ant,Bee,Cod,Cat,Dog,Yak,Cow,Hen,Koi,Hog,Jay,Kit]
+print(animals)
---
+++ challenge/challenge01.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+openme = open("challenge1.txt", "r")
+print(openme)
+
+
