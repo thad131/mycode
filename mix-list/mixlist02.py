e364d6272596909061571b2f699305c5db565da5
Thad Murillo <cheesecakethad@gmail.com>
2021-07-19 17:50:41 +0000

    working with lists

---
+++ mix-list/mixlist02.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+iplist = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
+
+#print ip addresses
+print("IP addresses are: ", iplist[3],iplist[4])
+print("IP Addresses are: \n",iplist[3],"\n",iplist[4])
