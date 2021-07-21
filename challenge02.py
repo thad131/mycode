787a958b8e181b6b82cc3fdc21fc735d20305ea1
Thad Murillo <cheesecakethad@gmail.com>
2021-07-21 17:39:48 +0000

    updating challenge 2

--- challenge02.py
+++ challenge02.py
@@ -7,7 +7,9 @@ import requests
 def main():
     jsondata = requests.get("http://api.open-notify.org/astros.json")
     astros = jsondata.json()
+    print("here is the data for astronauts in space: \n")
     print(astros['people'])
+    print("current status in readable format:")
     for naut in astros['people']:
         print(naut['name'] + " is onboard the " + naut['craft'])
     print("app complete")
